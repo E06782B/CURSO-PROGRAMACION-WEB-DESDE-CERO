6017bf05e39a4367235a9f095b152dbbb72e6150
E06782B <57377438+E06782B@users.noreply.github.com>
2023-04-14 10:03:54 -0500

    140420231
    nn

--- clases_php/reto2/descuentos/index.php
+++
@@ -1,5 +0,0 @@
-<form action="proceso_productos.php" method="post">
-    <label for="">Indicar cantidad de productos a ingresar:</label><br>
-    <input type="number" name="productsQuantity" id="">
-    <button type="submit">Enviar</button>
-</form>
